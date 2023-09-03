b8e3b71e4368a34e5e3bdfb3feb37667ce1d5c3f
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-09-04 00:34:49 +0900

    💄 ヘッダー完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Zen_Kaku_Gothic_New } from 'next/font/google'
+import Header from '@/compornent/Header/Header'
+import MainVisual from '@/compornent/MainVisual/MainVisual'
 
 const Zenkaku_new400 =Zen_Kaku_Gothic_New({
  weight: '400',
@@ -9,7 +11,8 @@ const Zenkaku_new400 =Zen_Kaku_Gothic_New({
 export default function Home() {
   return (
     <main className={Zenkaku_new400.className}>
-      
+      <Header />
+      <MainVisual />
     </main>
   )
 }
--- src/compornent/Header/Header.tsx
+++ src/compornent/Header/Header.tsx
@@ -3,38 +3,37 @@ import Link from 'next/link';
 import React from 'react';
 import styles from '../Header/Header.module.css';
 
-
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1>
-        <Link href="index.html">
+        <Link href="#">
           <Image
-            width={100}
+            width={200}
             height={100}
-            src="images/logo.jpg"
+            src="/header_logo.png"
             alt="YOKOHAMA RENOVEのロゴ"
           />
         </Link>
       </h1>
-      <nav>
-        <ul>
-          <li>
+      <nav className={styles.header__nav}>
+        <ul className="flex justify-end">
+          <li className={styles.header__li}>
             <Link href="#">ホーム</Link>
           </li>
-          <li>
+          <li className={styles.header__li}>
             <Link href="#">リノベの特徴</Link>
           </li>
-          <li>
+          <li className={styles.header__li}>
             <Link href="#">私たちのお約束</Link>
           </li>
-          <li>
+          <li className={styles.header__li}>
             <Link href="#">リノベ実績</Link>
           </li>
-          <li>
+          <li className={styles.header__li}>
             <Link href="#">資料請求</Link>
           </li>
-          <li>
+          <li className={styles.header__li_02}>
             <Link href="#">お問い合わせ</Link>
           </li>
         </ul>
