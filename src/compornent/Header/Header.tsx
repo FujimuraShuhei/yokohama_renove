186990d8c26c4cb28298ca4deae3a15992400187
shuhei_fujimura <shushu2525.1694@gmail.com>
2023-09-09 16:16:04 +0900

    ✨ ヘッダー調整、メインビジュアル追加

--- src/compornent/Header/Header.tsx
+++ src/compornent/Header/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={styles.header__nav}>
-        <h1>
+        <h1 className={styles.header__logo}>
           <Link href="#">
             <Image
               width={200}
@@ -31,6 +31,9 @@ const Header = () => {
             <Link href="#">リノベ実績</Link>
           </li>
           <li className={styles.header__li}>
+            <Link href="#">ブログ</Link>
+          </li>
+          <li className={styles.header__li_02}>
             <Link href="#">資料請求</Link>
           </li>
           <li className={styles.header__li_02}>
@@ -38,6 +41,21 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      <div className={styles.mainvisual}>
+        <div className={styles.mainvisual_textBox}>
+          <Image
+            className={styles.mainVisual_img}
+            src="/mainVisual_img.png"
+            width={229}
+            height={163}
+            alt="メインビジュアル イラスト"
+          />
+          <p>あなたの住みたい家に</p>
+          <h1>YOKOHAMA</h1>
+          <h1>RENOVE</h1>
+        </div>
+      </div>
+      ;
     </header>
   );
 };
--- src/compornent/MainVisual/MainVisual.tsx
+++ src/compornent/MainVisual/MainVisual.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from '../MainVisual/MainVisual.module.css';
+import Image from 'next/image';
 
 const MainVisual = () => {
-  return <div className={styles.MainVisual}></div>;
+  return;
+  <div className={styles.mainvisual}>
+    aaaaa
+  </div>;
 };
 
 export default MainVisual;
